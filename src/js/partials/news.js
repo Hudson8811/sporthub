618d30f535ddae2e062214c1763019ef1abc47a1
ArtZ <snickgem@gmail.com>
2020-06-11 13:48:14 +0400

    sections page

--- src/js/partials/news.js
+++ src/js/partials/news.js
@@ -2,6 +2,6 @@ $(document).ready(function () {
 	$('.news-choise__btn').click(function () {
 		$('.news-choise__btn').removeClass('news-choise__btn--active');
 		$(this).addClass('news-choise__btn--active');
-		$('.news-choise__items').removeClass('news-choise__items--active').eq($(this).index()).addClass('news-choise__items--active')
+		$('.news-choise__items').hide().eq($(this).index()).fadeIn(300);
 	});
 });
